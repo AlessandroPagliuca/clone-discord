9dcfeb773a75c675151699e2082117425955c216
Alessandro <alessandro2ap3@gmail.com>
2024-12-02 15:34:32 +0100

    fix responsive hamburger menu

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatIconModule, MatButtonModule, CommonModule],
   standalone: true,
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  protected isMenuOpen: boolean = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    // detect screen size changes
+    this.breakpointObserver.observe([
+      "(max-width: 992px)"
+    ]).subscribe((result: BreakpointState) => {
+      if (result.matches) {
+        this.isMenuOpen = false;
+      } else {
+        this.isMenuOpen = false;
+      }
+    });
+  }
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 
 }
