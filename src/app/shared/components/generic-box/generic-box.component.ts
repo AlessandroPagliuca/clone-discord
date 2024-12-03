296d82a540d73af9b58a0c4e248b40b2fc6b7e42
a.pagliuca <a.pagliuca@almaviva.it>
2024-12-03 12:32:34 +0100

    animation slide right to left

---
+++ src/app/shared/components/generic-box/generic-box.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generic-box',
+  imports: [],
+  standalone: true,
+  templateUrl: './generic-box.component.html',
+  styleUrl: './generic-box.component.scss'
+})
+export class GenericBoxComponent {
+
+}
