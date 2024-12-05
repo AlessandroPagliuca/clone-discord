8dabc506f82ecb916727c9d3a998ed362f4c1fdf
a.pagliuca <a.pagliuca@almaviva.it>
2024-12-05 16:21:50 +0100

    fix

--- src/app/core/views/home-page/home-page.component.ts
+++ src/app/core/views/home-page/home-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { GenericBoxComponent } from '../../../shared/components/generic-box/generic-box.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [GenericBoxComponent],
   standalone:true,
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
