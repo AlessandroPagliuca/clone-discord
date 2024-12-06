c6ed04175716cf72abe320ecc6b027842c64c245
Alessandro <alessandro2ap3@gmail.com>
2024-12-06 14:53:46 +0100

    fix

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { Country } from '../../../shared/models/country';
+import { MaterialModule } from '../../../shared/material.module';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [MaterialModule],
   standalone: true,
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  protected countriesList: Country[] = [
+    {
+      flag: 'en',
+      state: 'English',
+    },
+    {
+      flag: 'it',
+      state: 'Italiano',
+    },
+    {
+      flag: 'es',
+      state: 'Espanol',
+    },
+    {
+      flag: 'fr',
+      state: 'Francais',
+    },
+  ]
+  protected selectedCountry: string = this.countriesList[0].flag;
 
+  getStateByFlag(flag: string): string {
+    const country = this.countriesList.find(c => c.flag === flag);
+    return country ? country.state : '';
+  }
 }
--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -2,18 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-
-
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   imports: [
     CommonModule,
     MatIconModule,
     MatButtonModule,
+    MatSelectModule,
+    MatFormFieldModule,
   ],
-  exports:[
+  exports: [
     MatIconModule,
     MatButtonModule,
+    MatSelectModule,
+    MatFormFieldModule,
   ]
 })
 export class MaterialModule { }
